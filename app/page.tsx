24b8226859187fe39e20d9c36a84bd9fe0535089
Mani-Sankar01 <sm7532567@gmail.com>
2024-11-05 19:18:17 +0530

    added table with logic

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { AddReadingButton } from "@/components/AddReadingButton";
 import FilterableTable from "@/components/filterable-table";
+import FilterableTable2 from "@/components/FilterableTable2";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
@@ -14,15 +15,26 @@ export default async function Home() {
     },
   });
 
+  const formattedReadings = readings.map((reading) => ({
+    ...reading,
+    date: reading.date.toISOString().split("T")[0], // Format date as YYYY-MM-DD
+    status: reading.status.toLowerCase() as
+      | "active"
+      | "inactive"
+      | "cancelled"
+      | "failed",
+  }));
+
   console.log(readings);
 
   return (
     <div>
       <h2>
         Hiii
-        {JSON.stringify(readings)}
+        {JSON.stringify(formattedReadings)}
         <AddReadingButton />
-        <FilterableTable />
+        {/* <FilterableTable /> */}
+        <FilterableTable2 initialData={formattedReadings} />
       </h2>
     </div>
   );
