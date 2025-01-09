6067f5eb32c4c2baf71ef1ffce9bfcd2eb727a6c
Mani-Sankar01 <sm7532567@gmail.com>
2025-01-09 10:23:49 +0530

    added charts to the admin dashboard

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -3,6 +3,10 @@
 import React from "react";
 import { SidebarInset } from "@/components/ui/sidebar";
 import Header from "@/components/header";
+import { Component1 } from "@/components/chart1";
+import { Component4 } from "@/components/chart4";
+import { Component3 } from "@/components/chart3";
+import { Component2 } from "@/components/component2";
 
 function page() {
   return (
@@ -10,12 +14,18 @@ function page() {
       <Header breadcrumbs={[{ label: "Dashoard" }]} />
       <div className="flex flex-1 flex-col gap-4 p-4 pt-4">
         <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-          <div className="">qqq</div>
-          <div className="">www</div>
-          <div className="">eee</div>
+          <div className="">
+            <Component1 />
+          </div>
+          <div className="">
+            <Component2 />
+          </div>
+          <div className="">
+            <Component3 />
+          </div>
         </div>
         <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min">
-          rrr
+          <Component4 />
         </div>
       </div>
     </SidebarInset>
