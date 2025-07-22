8f4a5b182fd6d5ffd76c3c6b3e23089d20ae10a8
Mani-Sankar01 <sm7532567@gmail.com>
2025-07-22 19:05:26 +0530

    made some changes 7

--- app/admin/invoices/[id]/edit/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-import InvoiceForm from "@/components/invoice/invoice-form";
-
-export default function EditInvoicePage({ params }: { params: { id: string } }) {
-  const invoiceId = params.id;
-  return <InvoiceForm invoiceId={invoiceId} isEditMode={true} />;
-}
