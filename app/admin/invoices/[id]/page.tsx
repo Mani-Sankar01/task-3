66b4497c7d8f692c51f384f7df467234f54deeef
Mani-Sankar01 <sm7532567@gmail.com>
2025-07-22 20:53:31 +0530

    made some changes 11

---
+++ app/admin/invoices/[id]/page.tsx
@@ -0,0 +1,20 @@
+"use client";
+import InvoiceDetails from "@/components/invoice/invoice-details";
+import { SidebarInset } from "@/components/ui/sidebar";
+import Header from "@/components/header";
+
+export default function InvoiceDetailsPage({
+  params,
+}: {
+  params: { id: string };
+}) {
+  const invoiceId = params.id;
+  return (
+    <SidebarInset>
+      <Header breadcrumbs={[{ label: "Invoice Details" }]} />
+      <div className="flex flex-1 flex-col">
+        <InvoiceDetails invoiceId={invoiceId} />
+      </div>
+    </SidebarInset>
+  );
+}
