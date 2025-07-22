baca0da16da1736100e1a43242e188d26389172d
Mani-Sankar01 <sm7532567@gmail.com>
2025-07-22 17:27:50 +0530

    made some changes 6

--- app/admin/users/[id]/page.tsx
+++ app/admin/users/[id]/page.tsx
@@ -2,13 +2,7 @@
 import Header from "@/components/header";
 import { SidebarInset } from "@/components/ui/sidebar";
 import React from "react";
-import type { Metadata } from "next";
 import UserDetails from "@/components/users/user-details";
-
-export const metadata: Metadata = {
-  title: "User Details",
-  description: "View user details",
-};
 
 const Page = ({ params }: { params: { id: string } }) => {
   const userId = params.id;
